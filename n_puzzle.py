efe32bb1e73fe12978e0a1cafd10f47044303033
waremad <mycarline5@gmail.com>
2024-11-29 13:41:58 +0900

    分数の演算を実装

--- n_puzzle.py
+++ n_puzzle.py
@@ -11,6 +11,69 @@
 
 def int2frac(self):#int型を分数を表すstr型に
     if self == "":
-        return ""
+        return "0/1"
     out = str(self) + "/1"
     return out
+
+def str2frac(self):#/区切りのstr型をlistに
+    if self == "":
+        return [0,1]
+    out = self.split("/")
+    for i in range(len(out)):
+        out[i] = int(out[i])
+    return out
+
+def redufrac(self):#約分
+    if self == "" or self[0] == "0" or self[:2] == "-0":
+        return "0/1"
+    self = str2frac(self)
+    if self[0] < 0:
+        minus = self[1] > 0
+    else:
+        minus = self[1] < 0
+    for i in range(len(self)):
+        self[i] = abs(self[i])
+    n = min(self)
+    while not(n == 1 or self[0]%n + self[1]%n == 0):
+        n -= 1
+    out = str(self[0]//n) + "/" + str(self[1]//n)
+    if minus :
+        out = "-" + out
+    return out
+
+def addfrac(self,self2):#分数の足し算
+    self = str2frac(self)
+    self2 = str2frac(self2)
+    out = [
+        self[0] * self2[1] + self[1] * self2[0],
+        self[1] * self2[1]]
+    for i in range(len(out)):
+        out[i] = str(out[i])
+    out = "/".join(out)
+    out = redufrac(out)
+    return out
+
+def mulfrac(self,self2):#分数の掛け算
+    self = str2frac(self)
+    self2 = str2frac(self2)
+    out = [
+        self[0] * self2[0],
+        self[1] * self2[1]]
+    for i in range(len(out)):
+        out[i] = str(out[i])
+    out = "/".join(out)
+    out = redufrac(out)
+    return out
+
+def subfrac(self,self2):#分数の引き算
+    self2 = mulfrac(self2,"-1/1")
+    return addfrac(self,self2)
+
+def divfrac(self,self2):#分数の割り算
+    self2 = str2frac(self2)
+    self2[0], self2[1] = self2[1], self2[0]
+    for i in range(len(self2)):
+        self2[i] = str(self2[i])
+    self2 = "/".join(self2)
+    return mulfrac(self,self2)
+
--- test_n_puzzle.py
+++ test_n_puzzle.py
@@ -1,7 +1,55 @@
 from n_puzzle import *
 
 def test_int2frac():
-    assert int2frac() == ""
+    assert int2frac("") == "0/1"
     assert int2frac(0) == "0/1"
     assert int2frac(1) == "1/1"
     assert int2frac(1729) == "1729/1"
+
+def test_str2frac():
+    assert str2frac("") == [0,1]
+    assert str2frac("1/1") == [1,1]
+    assert str2frac("2/5") == [2,5]
+    assert str2frac("13/7") == [13,7]
+    assert str2frac("-3/11") == [-3,11]
+
+def test_redufrac():
+    assert redufrac("") == "0/1"
+    assert redufrac("1/1") == "1/1"
+    assert redufrac("4/2") == "2/1"
+    assert redufrac("57/3") == "19/1"
+    assert redufrac("7/91") == "1/13"
+    assert redufrac("-81/9") == "-9/1"
+    assert redufrac("-57/-3") == "19/1"
+
+def test_addfrac():
+    assert addfrac("","") == "0/1"
+    assert addfrac("1/1","1/1") == "2/1"
+    assert addfrac("1/2","-1/3") == "1/6"
+    assert addfrac("2/9","-4/5") == "-26/45"
+    assert addfrac("81/9","-21/-3") == "16/1"
+    assert addfrac("-5/3","7/9") == "-8/9"
+
+def test_mulfrac():
+    assert mulfrac("","") == "0/1"
+    assert mulfrac("1/1","1/1") == "1/1"
+    assert mulfrac("1/2","-1/3") == "-1/6"
+    assert mulfrac("2/9","-4/5") == "-8/45"
+    assert mulfrac("81/9","-21/-3") == "63/1"
+    assert mulfrac("-5/3","7/9") == "-35/27"
+
+def test_subfrac():
+    assert subfrac("","") == "0/1"
+    assert subfrac("1/1","1/1") == "0/1"
+    assert subfrac("1/2","-1/3") == "5/6"
+    assert subfrac("2/9","-4/5") == "46/45"
+    assert subfrac("81/9","-21/-3") == "2/1"
+    assert subfrac("-5/3","7/9") == "-22/9"
+
+def test_divfrac():
+    assert divfrac("","1/1") == "0/1"
+    assert divfrac("1/1","1/1") == "1/1"
+    assert divfrac("1/2","-1/3") == "-3/2"
+    assert divfrac("2/9","-4/5") == "-5/18"
+    assert divfrac("81/9","-21/-3") == "9/7"
+    assert divfrac("-5/3","7/9") == "-15/7"
